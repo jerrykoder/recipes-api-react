508999e6e046c8feb36307e098e889fd48d3af25
Luis-Gerardo <webdesigner@gerardobarroso.com>
2019-03-14 16:22:10 -0700

    Update files

--- custom.js
+++ custom.js
@@ -7,16 +7,16 @@ $('.scroll').click(function() {
 		location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
 		location.hostname == this.hostname
 	) {
-		var target = $(this.hash);
-		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		var target = $(this.hash)
+		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
 		if (target.length) {
 			$('html, body').animate(
 				{
-					scrollTop: target.offset().top - 200
+					scrollTop: target.offset().top - 50
 				},
 				1000
-			);
-			return false;
+			)
+			return false
 		}
 	}
-});
+})
